55d90f0aaa229acc8d50c15b79db7ad7aecfd8cb
zurctrebla <albertcruz@terra.com.br>
2021-10-19 02:09:44 +0000

    First Commit

---
+++ database/migrations/2021_10_18_230443_create_phones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('phone')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
